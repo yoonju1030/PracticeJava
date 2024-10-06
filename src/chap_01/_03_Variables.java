64e90de6ec7436b327117d84bd2eefc66f168028
yoonju1030 <allrealjgt@gmail.com>
2024-10-06 16:01:24 +0900

    variable

---
+++ src/chap_01/_03_Variables.java
@@ -0,0 +1,13 @@
+package chap_01;
+
+public class _03_Variables {
+    public static void main(String[] args) {
+        String name;
+        name = "shohoku";
+        String senshu = "sakuragi";
+        int number = 10;
+        System.out.println(name + " "+number+"번, "+senshu);
+
+        // double, char, boolean, final(constant)
+    }
+}
